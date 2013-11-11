69cda23b44dbd2ffb1d9f7e8a00269fe37163eae
Stephan Dilly <stephan@extrawurst.org>
2013-11-11 16:38:27 +0100

    initial implementation

---
+++ source/app.d
@@ -0,0 +1,25 @@
+import std.stdio;
+
+import std.algorithm;
+import xtea.XteaCrypto;
+
+void main()
+{ 
+	enum byte[] sourceData = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+	auto crypto = new XTEA([1,2,3,4], 64);
+
+	auto data = sourceData.dup;
+
+	writefln("data:\t\t%s",data);
+
+	crypto.Encrypt(data);
+
+	writefln("encrypted:\t%s",data);
+
+	crypto.Decrypt(data);
+
+	writefln("decrpypted:\t%s",data);
+
+	assert(equal(sourceData,data));
+}
---
+++ source/xtea/XteaCrypto.d
@@ -0,0 +1,98 @@
+module xtea.XteaCrypto;
+
+/++ 
+ +	XTEA helper class
+ +	see: http://en.wikipedia.org/wiki/XTEA
++/
+public class XTEA
+{
+	/// XTEA delta constant
+	private enum int DELTA = cast(int)0x9E3779B9;
+
+	/// Key - 4 integer
+	private const int[4] m_key;
+
+	/// Round to go - 64 are commonly used
+	private const int m_rounds;
+
+	/// c'tor
+	public this(int[4] _key, int _rounds)
+	{
+		m_key = _key;
+		m_rounds = _rounds;
+	}
+
+	/// Encrypt given byte array (length to be crypted must be 8 byte aligned)
+	public alias Crypt!(EncryptBlock) Encrypt;
+	/// Decrypt given byte array (length to be crypted must be 8 byte aligned)
+	public alias Crypt!(DecryptBlock) Decrypt;
+
+	///
+	private void Crypt(alias T)(byte[] _bytes, size_t _offset=0, int _count=-1)
+	{
+		if(_count == -1)
+			_count = _bytes.length - _offset;
+
+		assert(_count % 8 == 0);
+
+		for (size_t i = _offset; i < (_offset+_count); i += 8)
+			T(_bytes, i);
+	}
+
+	/// Encrypt given block of 8 bytes
+	public void EncryptBlock(byte[] _bytes, int _offset)
+	{
+		int v0 = ReadInt(_bytes, _offset);
+		int v1 = ReadInt(_bytes, _offset + 4);
+
+		int sum = 0;
+
+		for (int i = 0; i != m_rounds; i++)
+		{
+			v0 += ((v1 << 4 ^ cast(int)(cast(uint)v1 >> 5)) + v1) ^ (sum + m_key[sum & 3]);
+			sum += DELTA;
+			v1 += ((v0 << 4 ^ cast(int)(cast(uint)v0 >> 5)) + v0) ^ (sum + m_key[cast(int)(cast(uint)sum >> 11) & 3]);
+		}
+
+		StoreInt(v0, _bytes, _offset);
+		StoreInt(v1, _bytes, _offset + 4);
+	}
+
+	/// Decrypt given block of 8 bytes
+	public void DecryptBlock(byte[] _bytes, int _offset)
+	{
+		int v0 = ReadInt(_bytes, _offset);
+		int v1 = ReadInt(_bytes, _offset + 4);
+
+		auto sum = cast(int)(cast(uint)DELTA * cast(uint)m_rounds);
+
+		for (int i = 0; i != m_rounds; i++)
+		{
+			v1 -= ((v0 << 4 ^ cast(int)(cast(uint)v0 >> 5)) + v0) ^ (sum + m_key[cast(int)(cast(uint)sum >> 11) & 3]);
+			sum -= DELTA;
+			v0 -= ((v1 << 4 ^ cast(int)(cast(uint)v1 >> 5)) + v1) ^ (sum + m_key[sum & 3]);
+		}
+
+		StoreInt(v0, _bytes, _offset);
+		StoreInt(v1, _bytes, _offset + 4);
+	}
+
+	/// Read 32 bit int from buffer
+	private int ReadInt(byte[] _bytes, int _offset)
+	{
+		return (((_bytes[_offset++] & 0xff) << 0)
+				| ((_bytes[_offset++] & 0xff) << 8)
+				| ((_bytes[_offset++] & 0xff) << 16)
+				| ((_bytes[_offset] & 0xff) << 24));
+	}
+
+	/// Write 32 bit int from buffer
+	private void StoreInt(int _value, byte[] _bytes, int _offset)
+	{
+		auto unsignedValue = cast(uint)_value;
+		_bytes[_offset++] = cast(byte)(unsignedValue >> 0);
+		_bytes[_offset++] = cast(byte)(unsignedValue >> 8);
+		_bytes[_offset++] = cast(byte)(unsignedValue >> 16);
+		_bytes[_offset] = cast(byte)(unsignedValue >> 24);
+	}
+}
