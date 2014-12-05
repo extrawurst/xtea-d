ea1a0a86254e0d21bb7935d0f20961cda7090ad9
Stephan Dilly <stephan@extrawurst.org>
2014-12-05 13:48:56 +0100

    Update app.d
    adjust to changes in 0.1.0

--- source/app.d
+++ source/app.d
@@ -7,7 +7,7 @@ void main()
 { 
 	enum byte[] sourceData = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
-	auto crypto = new XTEA([1,2,3,4], 64);
+	auto crypto = XTEA([1,2,3,4], 64);
 
 	auto data = sourceData.dup;
 
